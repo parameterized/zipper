9e92c98892fb8dbec58fc67ff5b66c75dffed918
hachem2001 <abdeltif16@gmail.com>
2018-07-03 21:44:35 +0100

    Use love.keyboard.isScancodeDown instead of isDown.
    Reason : I use azerty, and probably other people do as well. There's also Dvorak lovers, qwertz users, and many different keyboard layouts. It's better to use scancodes to have them work for everyone instead!

--- player.lua
+++ player.lua
@@ -62,11 +62,11 @@ function player.update(dt)
 
     if not debugger.console.active and not chat.active then
         local dx, dy = 0, 0
-    	dx = dx + (love.keyboard.isDown('d') and 1 or 0)
-    	dx = dx + (love.keyboard.isDown('a') and -1 or 0)
-    	dy = dy + (love.keyboard.isDown('w') and -1 or 0)
-    	dy = dy + (love.keyboard.isDown('s') and 1 or 0)
-        local spd = player.spd*(love.keyboard.isDown('lshift') and 2.5 or 1)
+    	dx = dx + (love.keyboard.isScancodeDown('d') and 1 or 0)
+    	dx = dx + (love.keyboard.isScancodeDown('a') and -1 or 0)
+    	dy = dy + (love.keyboard.isScancodeDown('w') and -1 or 0)
+    	dy = dy + (love.keyboard.isScancodeDown('s') and 1 or 0)
+        local spd = player.spd*(love.keyboard.isScancodeDown('lshift') and 2.5 or 1)
         if not (dx == 0 and dy == 0) then
             local a = math.atan2(dx, dy) - math.pi/2
             player.body:applyForce(math.cos(a)*spd, -math.sin(a)*spd)
